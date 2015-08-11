a3f875d4a6432c84f7e98bd5effde4266e60fede
zhangjianbin <zhangjianbin@xiaomi.com>
2015-08-11 20:33:06 +0800

    add

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+(function (angular) {
+    'use strict';
+    angular.module('App', []).controller('ListCtrl', ['$scope', function ($scope) {
+            $scope.todoItems = [{
+                    content: '去医院看牙',
+                    done: false
+                }, {
+                    content: '周末买衣服',
+                    done: false
+                }, {
+                    content: '完成工作日报',
+                    done: true
+                }];
+
+            $scope.addItem = function () {
+                $scope.todoItems.push({
+                    content: $scope.content,
+                    done: false
+                });
+                $scope.content = '';
+            };
+            $scope.removeItem = function(item) {
+                var idx = $scope.todoItems.indexOf(item);
+                $scope.todoItems.splice(idx, 1);
+            };
+        }]);
+})(window.angular);
